2e72adf319c6d2499ebc48c676b6d6c96d63dca6
Efrin González <efringonzalez@gmail.com>
2017-10-09 15:30:10 +0200

    First commit
    Project creation and base configuration. Package and and main class setup.

---
+++ TShirtShop/src/main/java/com/tshirt/TShirtShopApplication.java
@@ -0,0 +1,12 @@
+package com.tshirt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TShirtShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TShirtShopApplication.class, args);
+	}
+}
---
+++ TShirtShop/src/test/java/com/employees/control/TShirtShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.employees.control;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TShirtShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
