3f83761e8600e68cf81f1d322172e310c5561f5f
Efrin González <efringonzalez@gmail.com>
2017-10-09 15:36:16 +0200

    Setting up Home Rest Controller.

---
+++ TShirtShop/src/main/java/com/tshirt/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.tshirt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/")
+	public String home(){
+		return "Welcome to the T-Shirt e-Shop";
+	}
+}
